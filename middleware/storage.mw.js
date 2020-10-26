986a7ebaa73bc12bbe68372da56a41ddc3b6d238
asafto <asafto@gmail.com>
2020-10-26 20:25:38 +0200

    changes to handle media with heroku

--- middleware/storage.mw.js
+++ middleware/storage.mw.js
@@ -1,15 +1,15 @@
 const multer = require('multer');
 const storage = multer.diskStorage(
-  // {
-  // destination: function (req, file, cb) {
-  //   // cb(null, 'public/');
-  //   cb(null, 'tmp/');
-  // },
-  // filename: function (req, filename, cb) {
-  //   const randomName = Math.random().toString(36).substring(7);
-  //   cb(null, randomName + '__' + filename.originalname);
-  // },
-  // }
+  {
+  destination: function (req, file, cb) {
+    // cb(null, 'public/');
+    cb(null, 'tmp/');
+  },
+  filename: function (req, filename, cb) {
+    const randomName = Math.random().toString(36).substring(7);
+    cb(null, randomName + '__' + filename.originalname);
+  },
+  }
 );
 
 const upload = multer({ storage: storage });
